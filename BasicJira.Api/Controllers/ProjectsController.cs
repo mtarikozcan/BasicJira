346234dd29357e82886dfb76c4594d599641f186
Mehmet Tarık Özcan <mehmettarik.ozcan@atlastek.com>
2026-07-06 11:51:47 +0300

    feat: implement first end-to-end CQRS feature
    - Added ProjectsController with CreateProject endpoint
    - Registered MediatR in the API layer
    - Connected controller to CreateProjectCommand via MediatR
    - Implemented CreateProjectCommandHandler using IAppDbContext
    - Successfully created the first Project through Swagger
    - Persisted project data to SQL Server using EF Core
    
    Learning:
    - Understood the complete request lifecycle:
      HTTP Request → Controller → MediatR → Command → Handler → DbContext → Database
    - Learned how ASP.NET Core Model Binding maps JSON requests to Command objects.
    - Learned the responsibilities of Controller, Command and Handler in a CQRS architecture.
    - Successfully completed the first end-to-end feature using Clean Architecture.

---
+++ BasicJira.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,31 @@
+﻿using BasicJira.Application.Projects.CreateProject;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicJira.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProjectsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProjectsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProjectCommand command)
+    {
+        var projectId = await _mediator.Send(command);
+
+        return Ok(projectId);
+    }
+
+
+}
+
+
+// Thin Controller, Fat Handler
+// controller request alıp mediatore gönder. response dondur. controllerda business logic yok. handlerda var.
--- BasicJira.Api/Program.cs
+++ BasicJira.Api/Program.cs
@@ -1,46 +1,45 @@
 using BasicJira.Application.Common.Interfaces;
+using BasicJira.Application.Projects.CreateProject;
 using BasicJira.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// registers AppDbContext in the Dependency Injection container.
-// whenever IAppDbContext is requested, AppDbContext will be provided.
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(
-            builder.Configuration.GetConnectionString("DefaultConnection")
-        );
-    // UseSqlServer methodu, Entity Framework Core'un SQL Server veritabanı sağlayıcısını kullanmasını sağlar.
+        builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
-
-// maps the abstraciton (IAppDbContext) to its concrete implementation
 
 builder.Services.AddScoped<IAppDbContext>(provider =>
     provider.GetRequiredService<AppDbContext>());
 
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(CreateProjectCommand).Assembly);
+});
 
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
