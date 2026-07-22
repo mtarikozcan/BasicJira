eb33d28e99d844308ed29caf75e506fc1be7f8b6
Mehmet Tarık Özcan <mehmettarik.ozcan@atlastek.com>
2026-07-22 10:11:46 +0300

    refactor: add application exception types

---
+++ BasicJira.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicJira.Application.Common.Exceptions;
+
+public sealed class NotFoundException : Exception
+{
+    public NotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public NotFoundException(string resourceName, object key)
+        : base($"{resourceName} with key '{key}' was not found.")
+    {
+    }
+}
